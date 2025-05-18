69243d39f5342da9c825799361e2972208aa69d7
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-18 18:32:26 +0200

    Update Person.java

--- src/main/java/org/house/projetjava8/model/Person.java
+++ src/main/java/org/house/projetjava8/model/Person.java
@@ -2,6 +2,7 @@ package org.house.projetjava8.model;
 import java.time.LocalDate;
 import java.util.List;
 
+
 public class Person {
     private int id;
     private String name;
@@ -15,4 +16,35 @@ public class Person {
         this.gender = gender;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
