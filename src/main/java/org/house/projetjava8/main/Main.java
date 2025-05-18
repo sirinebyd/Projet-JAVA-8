a2c6ff41b33547343fbd0dd56fda374e3f5bcb82
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-18 18:56:44 +0200

    Create Main.java

---
+++ src/main/java/org/house/projetjava8/main/Main.java
@@ -0,0 +1,44 @@
+import org.house.projetjava8.database.DatabaseManager;
+import org.house.projetjava8.model.*;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            DatabaseManager.connect();
+
+            
+            Person p1 = new Person(1, "Amine", 22, "Homme");
+            Person p2 = new Person(2, "Leïla", 25, "Femme");
+            DatabaseManager.getPersonDAO().add(p1);
+            DatabaseManager.getPersonDAO().add(p2);
+
+            
+            Room r1 = new Room(1, "Salle Alpha", 4, false); // non-mixte
+            Room r2 = new Room(2, "Salle Bêta", 6, true);  // mixte
+            DatabaseManager.getRoomDAO().add(r1);
+            DatabaseManager.getRoomDAO().add(r2);
+
+           
+            Bed b1 = new Bed(1, 1, true);
+            Bed b2 = new Bed(2, 1, true);
+            Bed b3 = new Bed(3, 2, true);
+            DatabaseManager.getBedDAO().add(b1);
+            DatabaseManager.getBedDAO().add(b2);
+            DatabaseManager.getBedDAO().add(b3);
+
+            
+            Occupation o1 = new Occupation(1, 1, 1, LocalDate.of(2025, 6, 1), LocalDate.of(2025, 6, 15));
+            Occupation o2 = new Occupation(2, 2, 3, LocalDate.of(2025, 6, 10), LocalDate.of(2025, 6, 20));
+            DatabaseManager.getOccupationDAO().add(o1);
+            DatabaseManager.getOccupationDAO().add(o2);
+
+            System.out.println("Données insérées avec succès !");
+            DatabaseManager.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
