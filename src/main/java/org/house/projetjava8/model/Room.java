20e8bcf0eea106bf034faf42347824d93195aa03
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-18 18:30:19 +0200

    Update Room.java

--- src/main/java/org/house/projetjava8/model/Room.java
+++ src/main/java/org/house/projetjava8/model/Room.java
@@ -1,11 +1,16 @@
 package org.house.projetjava8.model;
 import java.util.List;
-
 public class Room {
     private int id;
     private String name;
-    private List<Bed> beds;
-    private String genderRestriction;
-    private int minAge;
-    private int maxAge;
+    private int capacity;
+    private boolean isMixed; // true = mixte, false = non-mixte
+
+    public Room(int id, String name, int capacity, boolean isMixed) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.isMixed = isMixed;
+    }
+
 }
