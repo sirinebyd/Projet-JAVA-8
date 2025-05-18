82ff6fe55130ec99397c7c2b297a591c639caf06
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-18 18:31:35 +0200

    Update OccupancyRequest.java

--- src/main/java/org/house/projetjava8/model/OccupancyRequest.java
+++ src/main/java/org/house/projetjava8/model/OccupancyRequest.java
@@ -3,14 +3,22 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class OccupancyRequest {
-    private LocalDate minDate;
-    private LocalDate maxDate;
-    private boolean sameRoomRequired;
-    private List<PersonCriteria> personCriteriaList;
+    private int numberOfPeople;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean sameRoom;
+    private String gender;
+    private int minAge;
+    private int maxAge;
 
-    public static class PersonCriteria {
-        private String gender;
-        private int age;
+    public OccupancyRequest(int numberOfPeople, LocalDate startDate, LocalDate endDate, boolean sameRoom, String gender, int minAge, int maxAge) {
+        this.numberOfPeople = numberOfPeople;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.sameRoom = sameRoom;
+        this.gender = gender;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
     }
 
 }
