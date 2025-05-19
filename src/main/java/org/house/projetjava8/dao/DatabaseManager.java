e386bd378dd2d9f281edd9dcb8ee4d7934d2a60a
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-19 17:46:20 +0200

    Update DatabaseManager.java

--- src/main/java/org/house/projetjava8/dao/DatabaseManager.java
+++ src/main/java/org/house/projetjava8/dao/DatabaseManager.java
@@ -7,17 +7,4 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static Connection connection;
 
-    public static void connect(String dbPath) throws SQLException {
-        connection = DriverManager.getConnection("nom"+ chemin);
-    }
 
-    public static void disconnect() throws SQLException {
-        if (connection != null) {
-            connection.close();
-        }
-    }
-
-    public static Connection getConnection() {
-        return connection;
-    }
-}
