b6657e2e9aab61d36e846a4884d7639419702435
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-21 18:08:44 +0200

    Update HelloApplication.java

--- src/main/java/org/house/projetjava8/HelloApplication.java
+++ src/main/java/org/house/projetjava8/HelloApplication.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/house/projetjava8/MainView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Centre d’hébergement - Accueil");
         stage.setScene(scene);
         stage.show();
     }
