b030fe66d3da8f7afc99cf3dfa2e53209894f010
sirinebyd <sirine.bouayad1@gmail.com>
2025-05-21 16:51:15 +0200

    Update DatabaseManager.java

--- src/main/java/org/house/projetjava8/dao/DatabaseManager.java
+++ src/main/java/org/house/projetjava8/dao/DatabaseManager.java
@@ -1,36 +1,35 @@
-package org.house.projetjava8.dao; 
+package org.house.projetjava8.dao;
 
-import java.sql.Connection;        
-import java.sql.DriverManager;     
-import java.sql.SQLException;       
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DatabaseManager {     
+public class DatabaseManager {
+    private static final String DB_URL = "jdbc:sqlite:src/main/resources/db/database.db";
+   
+    private static Connection connection = null;
 
-    private static final String DB_URL = "jdbc:sqlite:src/main/resources/db/database.db"; 
-
-    private static Connection connection = null; 
-
-    public static Connection getConnection() {  
-        if (connection == null) {           
+    public static Connection getConnection() {
+        if (connection == null) {
             try {
                 connection = DriverManager.getConnection(DB_URL);
-                System.out.println("Connexion à la base réussie.");
-            } catch (SQLException e) {         
-                System.err.println("Erreur de connexion : " + e.getMessage());
+                System.out.println("Connexion réussie à la base de données");
+            } catch (SQLException e) {
+                System.out.println("Erreur lors de la connexion : " + e.getMessage());
             }
         }
-        return connection;                       
+        return connection;
     }
 
-    public static void closeConnection() {      
+    public static void closeConnection() {
         if (connection != null) {
             try {
-                connection.close();              
+                connection.close();
+                connection = null;
                 System.out.println("Connexion fermée.");
             } catch (SQLException e) {
-                System.err.println(" Erreur lors de la fermeture : " + e.getMessage());
+                System.out.println("Erreur lors de la fermeture de la connexion : " + e.getMessage());
             }
         }
     }
 }
-
