9faf3b738af0076d99fc7a27ad242837f3d58bee
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-22 19:43:59 +0200

    Update BedController.java

--- src/main/java/org/house/projetjava8/ui/BedController.java
+++ src/main/java/org/house/projetjava8/ui/BedController.java
@@ -1,42 +1,19 @@
-package org.house.projetjava8.ui;
+package org.house.projetjava8.controller;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.TextField;
 import org.house.projetjava8.model.Bed;
 import org.house.projetjava8.service.BedService;
 
 public class BedController {
+    private final BedService service = new BedService();
 
-    @FXML private Spinner<Integer> roomIdSpinner;
-    @FXML private Spinner<Integer> capacitySpinner;
-    @FXML private TableView<Bed> bedTable;
-
-    private final BedService bedService = new BedService();
+    @FXML private TextField capacityField;
 
     @FXML
-    public void initialize() {
-        roomIdSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1));
-        capacitySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 2, 1));
-        loadBeds();
-    }
-
-    @FXML
-    private void onAddBed() {
-        try {
-            Bed bed = new Bed(roomIdSpinner.getValue(), capacitySpinner.getValue());
-            bedService.addBed(bed);
-            loadBeds();
-        } catch (Exception e) {
-            showAlert("Error: " + e.getMessage());
-        }
-    }
-
-    private void loadBeds() {
-        bedTable.setItems(FXCollections.observableArrayList(bedService.getAllBeds()));
-    }
-
-    private void showAlert(String message) {
-        new Alert(Alert.AlertType.ERROR, message).show();
+    public void onSave() {
+        Bed bed = new Bed();
+        bed.setCapacity(Integer.parseInt(capacityField.getText()));
+        service.save(bed);
     }
 }
