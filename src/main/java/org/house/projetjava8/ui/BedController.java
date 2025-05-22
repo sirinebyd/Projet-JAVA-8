60cd901c10450d5b0f997176850dfb8803087483
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-22 19:56:09 +0200

    Update BedController.java

--- src/main/java/org/house/projetjava8/ui/BedController.java
+++ src/main/java/org/house/projetjava8/ui/BedController.java
@@ -8,12 +8,16 @@ import org.house.projetjava8.service.BedService;
 public class BedController {
     private final BedService service = new BedService();
 
+    @FXML private TextField labelField;
     @FXML private TextField capacityField;
+    @FXML private TextField roomIdField;
 
     @FXML
     public void onSave() {
         Bed bed = new Bed();
+        bed.setLabel(labelField.getText());
         bed.setCapacity(Integer.parseInt(capacityField.getText()));
+        bed.setRoomId(Integer.parseInt(roomIdField.getText()));
         service.save(bed);
     }
 }
