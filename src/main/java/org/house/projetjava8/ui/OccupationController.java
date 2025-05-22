82b65cd04dd772bf9f8fcf98f4ccce72a6a25680
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-22 19:56:53 +0200

    Update OccupationController.java

--- src/main/java/org/house/projetjava8/ui/OccupationController.java
+++ src/main/java/org/house/projetjava8/ui/OccupationController.java
@@ -1,6 +1,7 @@
 package org.house.projetjava8.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import org.house.projetjava8.model.Occupation;
 import org.house.projetjava8.service.OccupationService;
@@ -8,12 +9,20 @@ import org.house.projetjava8.service.OccupationService;
 public class OccupationController {
     private final OccupationService service = new OccupationService();
 
-    @FXML private TextField occupationInfoField;
+    @FXML private TextField personIdField;
+    @FXML private TextField bedIdField;
+    @FXML private DatePicker startDatePicker;
+    @FXML private DatePicker endDatePicker;
+    @FXML private TextField hasLeftField;
 
     @FXML
     public void onSave() {
-        Occupation occ = new Occupation();
-        occ.setInfo(occupationInfoField.getText());
-        service.save(occ);
+        Occupation occupation = new Occupation();
+        occupation.setPersonId(Integer.parseInt(personIdField.getText()));
+        occupation.setBedId(Integer.parseInt(bedIdField.getText()));
+        occupation.setStartDate(startDatePicker.getValue());
+        occupation.setEndDate(endDatePicker.getValue());
+        occupation.setHasLeft(Boolean.parseBoolean(hasLeftField.getText()));
+        service.save(occupation);
     }
 }
