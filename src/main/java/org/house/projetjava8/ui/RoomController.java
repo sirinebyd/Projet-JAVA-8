d5527b8af59a945b695803bd3f62c582329cc07a
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-22 19:55:46 +0200

    Create RoomController.java

---
+++ src/main/java/org/house/projetjava8/ui/RoomController.java
@@ -0,0 +1,25 @@
+package org.house.projetjava8.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.house.projetjava8.model.Room;
+import org.house.projetjava8.service.RoomService;
+
+public class RoomController {
+    private final RoomService service = new RoomService();
+
+    @FXML private TextField nameField;
+    @FXML private TextField genderRestrictionField;
+    @FXML private TextField minAgeField;
+    @FXML private TextField maxAgeField;
+
+    @FXML
+    public void onSave() {
+        Room room = new Room();
+        room.setName(nameField.getText());
+        room.setGenderRestriction(genderRestrictionField.getText());
+        room.setMinAge(Integer.parseInt(minAgeField.getText()));
+        room.setMaxAge(Integer.parseInt(maxAgeField.getText()));
+        service.save(room);
+    }
+}
