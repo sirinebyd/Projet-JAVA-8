dc631964528c1cef0c1881e006ee525438d069e1
W4v3-cmd <charlymayeur3@gmail.com>
2025-05-22 18:23:56 +0200

    RoomService add

---
+++ src/main/java/org/house/projetjava8/service/RoomService.java
@@ -0,0 +1,23 @@
+package org.house.projetjava8.service;
+
+import org.house.projetjava8.dao.RoomDAO;
+import org.house.projetjava8.model.Room;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class RoomService {
+    private final RoomDAO roomDAO;
+
+    public RoomService(RoomDAO roomDAO) {
+        this.roomDAO = roomDAO;
+    }
+
+    public List<Room> getAllRooms() throws SQLException {
+        return roomDAO.getAll();
+    }
+
+    public Room getRoomById(int id) throws SQLException {
+        return roomDAO.getById(id);
+    }
+}
