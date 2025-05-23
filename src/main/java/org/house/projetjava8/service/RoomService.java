ff48b623491d64732263cd2769aa270b09ac930b
MatisseJacquet <153311899+JacquetMatisse@users.noreply.github.com>
2025-05-23 04:19:40 +0200

    Update RoomService.java

--- src/main/java/org/house/projetjava8/service/RoomService.java
+++ src/main/java/org/house/projetjava8/service/RoomService.java
@@ -2,6 +2,9 @@ package org.house.projetjava8.service;
 
 import org.house.projetjava8.dao.RoomDAO;
 import org.house.projetjava8.model.Room;
+import org.house.projetjava8.model.Bed;
+import org.house.projetjava8.service.BedService;
+import org.house.projetjava8.dao.BedDAO;
 
 import java.util.List;
 import java.sql.SQLException;
@@ -10,6 +13,8 @@ import org.house.projetjava8.service.BedService;
 
 public class RoomService {
     private final RoomDAO dao = new RoomDAO();
+    private final BedService bedService = new BedService();
+    private final BedDAO bedDao = new BedDAO()
 
     public List<Room> getAll() {
         try {
@@ -45,14 +50,14 @@ public class RoomService {
     public boolean deleteRoomIfPossible(int roomId) {
     List<Bed> beds = BedService.getBedsByRoom(roomId);
     for (Bed b : beds) {
-        if (BedDAO.isBedInUse(b.getId())) {
+        if (BedDao.isBedInUse(b.getId())) {
             throw new IllegalStateException("At least one bed in this room is occupied.");
         }
     }
     // Supprimer tous les lits avant la salle
     for (Bed b : beds) {
-        BedDAO.deleteBed(b.getId());
+        BedDao.deleteBed(b.getId());
     }
-    return RoomDAO.deleteRoom(roomId);
+    return dao.deleteRoom(roomId);
 }
 }
